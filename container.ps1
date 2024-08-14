d979d530fd0b05c75da8dfa27489f9544e385ce7
unknown <sahilphule2003@gmail.com>
2024-08-14 19:40:03 +0530

    removed rebuilding of nginx image

--- container.ps1
+++ container.ps1
@@ -1,9 +1,6 @@
-#!bin/sh
-cd /home/ec2-user/quizstar
-docker-compose down;
+docker-compose down
 docker rmi sahilphule0710/django-app
 # docker rmi nginx
 # docker rmi mysql
-# docker rmi -f $(docker images -aq)
 docker-compose -f docker-compose.yaml up -d
 docker cp ./nginx.conf nginx-service:/etc/nginx/conf.d/nginx.conf
