208371cece9a3a9b1a41b083e106a9d7bb1e3ad6
unknown <sahilphule2003@gmail.com>
2024-08-14 22:30:21 +0530

    s

--- container.sh
+++ container.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 cd /home/ec2-user/quizstar-portal
-docker-compose -f docker-compose.yaml down;
-docker rmi sahilphule0710/django-app
+docker-compose -f docker-compose.yml down;
+docker rmi django-app
 # docker rmi nginx
 # docker rmi mysql
 # docker rmi -f $(docker images -aq)
-docker-compose -f docker-compose.yaml up -d
-docker cp ./nginx.conf nginx-service:/etc/nginx/conf.d/nginx.conf
+docker-compose -f docker-compose.yml up -d
+docker cp ./default.conf nginx-service:/etc/nginx/conf.d/default.conf
