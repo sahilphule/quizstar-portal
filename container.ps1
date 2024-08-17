c8b001110899e70d5f424e42231748a1a5e8ad06
unknown <sahilphule2003@gmail.com>
2024-08-17 23:26:58 +0530

    deleted old django-app files

--- container.ps1
+++
@@ -1,6 +0,0 @@
-docker-compose -f docker-compose.yml down
-docker rmi sahilphule0710/django-app
-# docker rmi nginx
-# docker rmi mysql
-docker-compose -f docker-compose.yml up -d
-docker cp ./default.conf nginx-service:/etc/nginx/conf.d/default.conf
--- container.sh
+++
@@ -1,9 +0,0 @@
-#!bin/sh
-cd /home/ec2-user/quizstar-portal
-docker-compose -f docker-compose.yml down;
-docker rmi django-app
-# docker rmi nginx
-# docker rmi mysql
-# docker rmi -f $(docker images -aq)
-docker-compose -f docker-compose.yml up -d
-docker cp ./default.conf nginx-service:/etc/nginx/conf.d/default.conf
